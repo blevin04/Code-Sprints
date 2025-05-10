8ff592a05ff64e1beac27500a598a7ec0579502d
blevin04 <jamestonito04@gmail.com>
2025-05-10 12:17:38 +0300

    fixed tax_error

--- problems/easy/tax_error/__tests__/tax_error.py
+++ problems/easy/tax_error/__tests__/tax_error.py
@@ -1,4 +1,9 @@
-from problems.easy.tax_error import calculate_tax
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from tax_error import calculate_tax
 
 testcase = [10000,0,-500]
 expected = [1500,0,0]
--- problems/easy/vowels/__tests__/vowels.py
+++ problems/easy/vowels/__tests__/vowels.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from problems.easy.vowels.vowels import count_vowels
+from vowels import count_vowels
 
 testWords = [
     "home",
